2cad6fa875ff6299f52882ceef43650744b90712
Meehoi <beam_baimon@hotmail.com>
2018-10-14 13:09:20 +0700

    First Commit
    Some Feature is already made - on a long way to become shooter!

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    private float m_OffsetZ;
+
+    // Use this for initialization
+    private void Start()
+    {
+        m_OffsetZ = (transform.position - target.position).z;
+        transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = target.position + Vector3.forward * m_OffsetZ;
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D body;
+    public float moveSpeed;
+    public float turnSpeed;
+
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        body.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime, 0.8f),
+                                    Mathf.Lerp(0, Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime, 0.8f));
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rotation = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+        
+    }
+
+}
